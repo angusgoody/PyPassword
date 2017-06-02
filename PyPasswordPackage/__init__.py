00dca0f22c446e8b321697561434b0d5aa7101aa
Angus Goody <angus.goody@gmail.com>
2017-06-02 18:49:44 +0100

    Added package and init file

---
+++ PyPasswordPackage/__init__.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+
+#Angus Goody
+#PyPassword 2.0
+#02/06/17
+
+#Main Init File
+
+#==================================(IMPORTS)=============================
+from tkinter import *
+from tkinter import messagebox
+
+#==================================(Window setup)=============================
+window=Tk()
+
+#==================================(END)=============================
+window.mainloop()
