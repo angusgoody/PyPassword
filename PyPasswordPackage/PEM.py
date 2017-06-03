5e58fe8aae34772d855d90b17b918761c4f60b11
Angus Goody <angus.goody@gmail.com>
2017-06-03 10:57:36 +0100

    Added modules

---
+++ PyPasswordPackage/PEM.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+
+#Angus Goody
+#PyPassword 2.0
+#02/06/17
+
+#Password Encryption Module (PEM)
+
+"""
+This file is the Encryption module
+this is where all the encryption and
+decryption of the data will happen
+to save it from being in the main __init__ file
+"""
+#==================================(Functions)=============================
+
+#==================================(Classes)=============================
---
+++ PyPasswordPackage/PyUi.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+
+#Angus Goody
+#PyPassword 2.0
+#02/06/17
+
+#Main User Interface Module
+
+
+"""
+This file is a custom user interface module
+that uses tkinter. It is used so all the
+user interface code does not take up the
+main __init__ file.
+"""
+
+#==================================(IMPORTS)=============================
+from tkinter import *
