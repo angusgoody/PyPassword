564653d6dd31cf164b2215fd03323ff65915398c
Angus Goody <angus.goody@gmail.com>
2017-06-03 11:12:42 +0100

    Added MainLabel and MainFrame

--- PyPasswordPackage/PyUi.py
+++ PyPasswordPackage/PyUi.py
@@ -17,3 +17,27 @@ main __init__ file.
 
 #==================================(IMPORTS)=============================
 from tkinter import *
+
+#==================================(FUNCTIONS)=============================
+
+#==================================(CLASSES)=============================
+
+class mainFrame(Frame):
+	"""
+	The Main Frame class is a modified tkinter Frame
+	that adds more customization and flexibility, for
+	example changing colour and bindings etc.
+	"""
+	def __init__(self,parent,**kwargs):
+		Frame.__init__(self,parent,kwargs)
+
+class mainLabel(Label):
+	"""
+	The mainLabel class is similar to the mainFrame
+	in the fact it is based off an existing tkinter
+	widget. It adds more options to the Label widget
+	which makes it easier to change colours and fonts
+	and also hover bindings etc.
+	"""
+	def __init__(self,parent,**kwargs):
+		Label.__init__(self,parent,kwargs)
