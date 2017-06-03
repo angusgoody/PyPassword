be292388530d5c0118b97a2e363efb96635529ed
Angus Goody <angus.goody@gmail.com>
2017-06-03 16:05:10 +0100

    Improved encryption system for pods

--- PyPasswordPackage/PEM.py
+++ PyPasswordPackage/PEM.py
@@ -17,16 +17,28 @@ to save it from being in the main __init__ file
 from Crypto.Cipher import AES
 import pickle
 
-#==================================(LISTS)=============================
 #==================================(FUNCTIONS)=============================
 
 def pad(text):
+	"""
+	For AES encryption keys and data must
+	be in multiples of 16 so the pad function
+	adds padding to make it the right length
+	"""
 	return text +((16-len(text) % 16)*"\n")
 
-def cipher(plainText,key):
-	text=pad(plainText)
+def chiper(plainText, key):
+	"""
+	The cipher function encrypts
+	data with an AES key
+	"""
+	text=pad(str(plainText))
 	encrypted=key.encrypt(text)
 	return encrypted
+
+def decrypt(data,key):
+	return key.decrypt(data).rstrip()
+
 #==================================(Classes)=============================
 class dataPod:
 	"""
@@ -35,10 +47,29 @@ class dataPod:
 	a certain account. It contains account name,password
 	etc
 	"""
-	def __init__(self,podName,dataDict,master):
+	def __init__(self,podName,podVault,master):
 		self.podName=podName
 		self.master=master
-		self.dataDict=dataDict
+		self.podVault=chiper(podVault,self.master.masterKey)
+
+	def __string__(self):
+		return self.podName
+
+	def getPodData(self):
+		"""
+		This method returns just the data inside the pod
+		which is known as the pod vault and contains the
+		private information
+		"""
+		return self.podVault
+
+	def getPod(self):
+		"""
+		This method returns a dictionary which
+		contains the podName and podVault
+		"""
+		return {self.podName:self.podVault}
+
 
 class masterPod:
 	"""
@@ -47,23 +78,32 @@ class masterPod:
 	key
 	"""
 	def __init__(self,masterKey):
-		self.masterKey=masterKey
+		#Create an AES encryption key
+		self.masterKey=AES.new(pad(masterKey))
 		self.pods={}
 
 	def addPod(self,podName,dataDict):
+		"""
+		The add pod function will create
+		a dataPod with the master pod
+		as the master and add it to the master pod
+		collection
+		"""
 		#Create pod instance
 		newPod=dataPod(podName,dataDict,self)
-		#Encrypt Pod
-		encryptionKey=AES.new(pad(podName))
-		encryptedPod=cipher(str(newPod),encryptionKey)
+		newPodData=newPod.getPodData()
 		#Add to master pod
-		self.pods[podName]=encryptedPod
+		self.pods[podName]=newPodData
 
 	def savePods(self,fileName):
+		"""
+		The save pods method will encrypt the dictionary
+		that contains all the pods and export it to a file
+		using Pickle
+		"""
 
 		#Encrypt pods
-		encryptionKey=AES.new(pad(self.masterKey))
-		encryptedMaster=cipher(str(self.pods),encryptionKey)
+		encryptedMaster=chiper(str(self.pods), self.masterKey)
 
 		#Pickle File
 		pickle.dump(encryptedMaster, open( fileName, "wb" ) )
@@ -72,19 +112,14 @@ class masterPod:
 	def loadPods(self,fileName):
 
 		encryptedMaster = pickle.load( open( fileName, "rb" ) )
-		encryptionKey=AES.new(pad(self.masterKey))
-
-		decryptedPods=encryptionKey.decrypt(encryptedMaster).rstrip()
+		decryptedPods=self.masterKey.decrypt(encryptedMaster).rstrip()
 		dataDict=eval(decryptedPods)
 		print(dataDict)
-		
+
 
 newMaster=masterPod("angus")
 #newMaster.addPod("Amazon",{"Name":"Amazon","Password":"Turtle"})
 #newMaster.savePods("data.p")
 newMaster.loadPods("data.p")
-
-
-
 
 
