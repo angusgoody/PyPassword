482a8da80e84aa8081eff74edb1687a038161ff6
Angus Goody <angus.goody@gmail.com>
2017-10-05 10:49:48 +0100

    Starting buttons again

--- PyPasswordPackage/__init__.py
+++ PyPasswordPackage/__init__.py
@@ -231,15 +231,6 @@ homeMainFrame.pack(expand=True,fill=BOTH)
 #Fake Notebook View
 #todo fake notebook
 
-homeListboxBar=fixedButtonBar(homeMainFrame,bg="#43538A")
-homeListboxBar.pack(fill=X)
-
-def temp():
-	print("Hi")
-homeListboxBar.addButton("All",temp)
-homeListboxBar.addButton("Login",temp)
-homeListboxBar.addButton("Notes",temp)
-
 homePodListbox=searchListbox(homeMainFrame)
 homePodListbox.addSearchWidget(homeSearchEntry,resultVar=homeSearchVar)
 homePodListbox.pack(expand=True,fill=BOTH)
