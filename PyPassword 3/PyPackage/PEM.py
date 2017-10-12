cd5ec2cd4d2c843250df05e493fd9d9ee08684ee
Angus Goody <angus.goody@gmail.com>
2017-10-12 12:09:04 +0100

    Added PyPassword 3 branch

---
+++ PyPassword 3/PyPackage/PEM.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+#Angus Goody
+#12/10/17
+#PyPassword 3
+
+"""
+This file is the main Encryption module for PyPassword,
+all the encrypting and decrypting will happen in this file.
+"""
+
+#==========IMPORTS==========
